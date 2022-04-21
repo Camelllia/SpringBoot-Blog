d21a47b1586e21877c8312bddac6eb4bee9a119d
Nomadzz <82443328+Camelllia@users.noreply.github.com>
2022-04-22 02:19:52 +0900

    Update: Create RecommendedBoard for Main(index)Page

--- Blog/src/main/java/MyBlog/Blog/controller/HomeController.java
+++ Blog/src/main/java/MyBlog/Blog/controller/HomeController.java
@@ -1,14 +1,32 @@
 package MyBlog.Blog.controller;
 
+import MyBlog.Blog.model.board;
+import MyBlog.Blog.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Random;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    BoardRepository boardRepository;
+
     // 블로그 홈
     @GetMapping()
-    public String index() {
+    public String index(Model model,  @RequestParam(required = false) Long randomId) {
+
+        // 랜덤으로 추천 글 생성
+        Random random = new Random();
+        Long pivot = Long.valueOf(boardRepository.findAll().size());
+        randomId = (long) (Math.random() * pivot + 1);
+        board recommendedBoard = boardRepository.findById(randomId).orElse(null);
+        model.addAttribute("recommendedBoard", recommendedBoard);
+        
         return "index";
     }
 }
